4f827357e87cc4ea79dd0cc3ec1b66092ca883c2
Daehwang Kim <daehwang.kim@navercorp.com>
2018-04-12 23:47:35 +0900

    Lab 강의 (9) 실습

---
+++ exercise/lab-09-02.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import numpy as np
+
+tf.set_random_seed(777) # for reproducibility
+learning_rate = 0.1
+
+x_data = [[0, 0],
+          [0, 1],
+          [1, 0],
+          [1, 1]]
+y_data = [[0],
+          [1],
+          [1],
+          [0]]
+x_data = np.array(x_data, dtype=np.float32)
+y_data = np.array(y_data, dtype=np.float32)
+
+X = tf.placeholder(tf.float32, [None, 2])
+Y = tf.placeholder(tf.float32, [None, 1])
+
+# 주의!! Layer가 2개 이므로, 여기서는 output의 값을 2로 하였음
+W1 = tf.Variable(tf.random_normal([2, 2]), name='weight1')
+b1 = tf.Variable(tf.random_normal([2]), name='bias1')
+layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+
+# 주의!! 앞 Layer의 결과값 사이즈(2)와 출려값의 사이즈(1)에 맞춰야 함
+W2 = tf.Variable(tf.random_normal([2, 1]), name='weight2')
+b2 = tf.Variable(tf.random_normal([1]), name='bias2')
+hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+
+# cost/loss function
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+
+train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Accuracy computation
+# True if hypothesis > 0.5 else False
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+# Lauch graph
+with tf.Session() as sess:
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        sess.run(train, feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run([W1, W2]))
+
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
+
+
